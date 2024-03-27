c985821be6ccd928bf781d8bca52a2069598d58e
ChinKhai <janchinkhai@gmail.com>
2024-03-27 15:50:17 +0800

    import bootstrap
    upload header, footer, frontend, routes

--- CI/application/config/config.php
+++ CI/application/config/config.php
@@ -23,7 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+
+if($_SERVER['HTTP_HOST']=="mytesting.local"){
+$config['base_url'] = 'http://mytesting.local/index.php/';
+}else{
+    $config['base_url']='http://mytesting.com.my/index.php/';
+}
 
 /*
 |--------------------------------------------------------------------------
---
+++ CI/application/controllers/Frontend.php
@@ -0,0 +1,22 @@
+<?php
+class Frontend extends CI_Controller{
+    public function home(){
+        $this->load->view("header");
+    }
+
+    public function add_product(){
+        $this->load->view('header');
+        $this->load->view('add_product');
+
+    }
+
+    public function update_product(){
+        echo "<h1>Update Product</h1>";
+    }
+
+    public function view_product(){
+        echo "<h1>Product Details</h1>";
+    }
+}
+
+?>
